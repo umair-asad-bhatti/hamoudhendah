67b63de5a46e0598a1ea9b79f4cc46b6f159dd7b
umair-asad-bhatti <umaircyber6@gmail.com>
2024-06-14 21:30:13 +0500

    fixed cores issue

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+var cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const router = require("./router/Router")
 const connectDB = require('./db/connect')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use("/api/v1/", router)
+app.use(cors())
 // Use PORT provided in environment or default to 3000
 const port = process.env.PORT || 3000;
 // Listen on `port` and 0.0.0.0
